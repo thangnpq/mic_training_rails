00a624905a0c88e3d45df7a261fc32da4ba179ad
Eric Nguyen <thang.nguyen2196@gmail.com>
2017-06-13 11:26:50 +0700

    format layout

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@ class Article < ApplicationRecord
   validates :title, presence: true,length: { minimum: 5 }
   self.locking_column = :lock_article_column
   belongs_to :user
-  has_attached_file :image, styles: { medium: "250x250>", thumb: "100x100>" }
+  has_attached_file :image, styles: { high: "600x400>",medium: "250x250>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   def self.search search
     where("title ILIKE :search OR text ILIKE :search", search: "%#{search}%")
